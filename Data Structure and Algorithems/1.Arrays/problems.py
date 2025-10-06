95b544cd8c09452671ebc7b20b4933bc6a396ae5
Nayan Prajapti <info.nayan1323@gmail.com>
2025-10-06 22:40:52 +0530

    intial commit

--- Data Structure and Algorithems/1.Arrays/problems.py
+++ Data Structure and Algorithems/1.Arrays/problems.py
@@ -91,3 +91,65 @@
 # for i in arr:
 #     reversed_arr.insert(0, i)
 # print(reversed_arr)
+
+
+
+# 9.Find the index of a given element.
+# arr = [21, 54, 8, 46, 12 ,45]
+# element = 46
+# for i in range(len(arr)):
+#     if arr[i] == element:
+#         print(f'Element {element} found at index {i}')
+#         break
+
+
+
+# 10.Count frequency of each element.
+# arr = [21, 54, 8, 46, 12 ,45, 21, 54, 8]
+# frequency = {}
+# for i in arr:
+#     if i in frequency:
+#         frequency[i] += 1
+#     else:
+#         frequency[i] = 1
+# print(frequency)
+
+
+# extra. Remove duplicates from an array.
+# arr = [21, 54, 8, 46, 12 ,45, 21, 54, 8]
+# unique_arr = []
+# for i in arr:
+#     if i not in unique_arr:
+#         unique_arr.append(i)    
+# print(unique_arr)
+
+
+
+# -----------------------🟡 Intermediate Level (Patterns & Logic)
+
+
+# 11.Move all zeros to the end.
+# arr = [0, 1, 0, 3, 12]
+# non_zero_arr = [i for i in arr if i != 0]
+# zero_count = arr.count(0)
+# result = non_zero_arr + [0]*zero_count
+# print(result)
+# another method
+# arr = [0, 1, 0, 3, 12]
+# result = []
+# for i in arr: 
+#     if i != 0:
+#         result.append(i)
+# for i in arr:
+#     if i == 0:    
+#         result.append(i)
+# print(result)
+
+
+
+# 12.Rotate an array by k steps.
+arr = [1, 2, 3, 4, 5, 6, 7]
+k = 3
+k = k % len(arr)  # In case k is greater than array length    
+rotated_arr = arr[-k:] + arr[:-k]
+print(rotated_arr)
